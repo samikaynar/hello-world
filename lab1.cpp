e72a8d5f6a372a1464fe5b43ca1dffc213e5a6d8
Sami <samikaynr41@gmail.com>
2024-08-30 10:42:55 -0500

    test4

--- lab1.cpp
+++ lab1.cpp
@@ -3,8 +3,7 @@
 int main(){
 
     std::cout << "232323" << std::endl;
-    std::cout << "asdads" << std::endl;
-    std::cout << "asd12321ads" << std::endl;
+    std::cout << "2" << std::endl;
 
 
 
